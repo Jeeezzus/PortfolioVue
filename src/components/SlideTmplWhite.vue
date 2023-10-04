d7f9ab90be904cf042fc606c72c85986e8cabc1c
Gauthier Gendreau <gauthier.gendreau@outlook.com>
2023-10-04 16:17:34 +0200

    started mobile version

--- src/components/SlideTmplWhite.vue
+++ src/components/SlideTmplWhite.vue
@@ -8,6 +8,9 @@
       <div class="column2">
         <h2 class="slide-title" :style="{color: titlecolor}">{{ title }}</h2>
       <p class="slidecap">{{ caption }}</p>
+      <router-link :to="routerPath">
+      <a class="btn btn-1">Hover me</a>
+      </router-link>
       </div>
     </div>
   </div>
@@ -20,6 +23,7 @@ export default {
     imagePath: String,
     caption: String,
     titlecolor: String,
+    routerPath: String,
   },
 };
 </script>
@@ -28,89 +32,87 @@ export default {
 /* Add your CSS styles for the slide template here */
 .slide-tmpl {
   color: white;
+  background-color: #333;
   position: relative;
-  text-align: center;
+  text-align: justify;
   overflow: hidden; /* Ensure the image doesn't overflow the container */
 }
 
 /* Style the image container */
 .image-container {
-  width: 100%;
-  height: 100%;
   object-fit: cover;
 }
 
 /* Style the image to be fullscreen */
 .full-image {
-  width: 100%M !important;
   aspect-ratio: 1;
   object-fit: cover;
 }
 
 /* Style the title */
 .slide-title {
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
+  text-align: justify;
   background-color: transparent;
-  padding: 10px;
   margin: 0;
-  font-size: 3em;
+  font-size: 2.5em;
   background-color: hsla(0, 0%, 0%, 0.25);
 }
 
 .slidecap {
-  position: absolute;
-  bottom: 0; /* Adjust this value as needed */
   background-color: transparent; /* Set the background color to transparent */
-  margin: 0;
-  padding: 10px;
-  font-size: 1.5em;
+  margin-top: 10px;
+  font-size: 1.25em;
   background-color: hsla(0, 0%, 0%, 0.25);
-}
-
-.reflection {
-  position: absolute;
-  bottom: -50%; /* Adjust the reflection position as needed */
-  left: 0;
-  right: 0;
-  height: 50%; /* Height of the reflection */
-  background: linear-gradient(to bottom, rgba(255, 255, 255, 0.4), rgba(255, 255, 255, 0));
-  transform: scaleY(-1); /* Flip the reflection content vertically */
-  opacity: 0.7; /* Adjust the opacity of the reflection */
 }
 
 .two-column-layout {
   object-fit: cover;
   display: flex;
-  width: 100% !important;
-  height: 100% !important; /* Set the height of the container as needed */
 }
 
 .column2 {
-  object-fit: cover;
+
   flex: 10;
-  align-items: center;
-  width: 100% !important;
-  height: 100% !important;
 }
 
 .column1 {
-  object-fit: cover;
   flex: 15;
-  align-items: center;
-  width: 100% !important;
-  height: 100% !important;
+}
+
+.btn {
+ position: absolute;
+  bottom: 30px;
+  margin: 10px;
+  padding: 30px;
+  text-align: center;
+  text-transform: uppercase;
+  transition: 0.5s;
+  background-size: 200% auto;
+  color: white;
+  box-shadow: 0 0 20px #eee;
+  border-radius: 10px;
+ }
+
+ .btn:hover {
+  background-position: right center; /* change the direction of the change here */
+}
+
+.btn-1 {
+  background-image: linear-gradient(to right, #f6d365 0%, #fda085 51%, #f6d365 100%);
 }
 @media (max-width: 750px) {
   .two-column-layout {
     flex-direction: column; /* Change to vertical layout */
-    align-items: center; /* Center horizontally in vertical layout */
+    align-items: top; /* Center horizontally in vertical layout */
   }
 
   .slide-title{
     height: 80vh;
   }
+
+  .slide-tmpl{
+    height: 80vh;
+  }
+
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,31 +2,21 @@
   <div class="home">
   <div class="carou">
   <carousel-3d :controls-visible="true" :controls-prev-html="'&#10092; '" :controls-next-html="'&#10093;'" 
-               :controls-width="30" :controls-height="60" :height="heightInPixels" :width="heightInPixels*1.75"  >
+               :controls-width="40" :controls-height="80" :height="heightInPixels" :width="heightInPixels*1.75" :display="5">
   <slide :index="0" class="slide">
-    <router-link to="/DVB">
-    <SlideTmplWhite title="InMoov and Jasper" caption='Development then project management for DavinciBot in the creation of the humanoid robots "InMoov" and "JASPER"' image-path="/DVB/InMoov_Working.png" textcolor="white"/>
-    </router-link>
+    <SlideTmplWhite title="InMoov and Jasper" routerPath='/dvb' caption='Development then project management for DavinciBot in the creation of the humanoid robots "InMoov" and "JASPER"' image-path="/DVB/InMoov_Working.png" textcolor="white"/>
   </slide>
   <slide :index="1" class="slide">
-    <router-link to="/ExoTouch">
-    <SlideTmplWhite title="ExoTouch haptic gloves" caption='Creation of haptic gloves with force feedback for virtual reality, DIY and inexpensive.' image-path="/ExoTouch/ExoTouchV3.jpeg" textcolor="white"/>
-    </router-link>
+    <SlideTmplWhite title="ExoTouch haptic gloves" routerPath='/exotouch' caption='Creation of haptic gloves with force feedback for virtual reality, DIY and inexpensive.' image-path="/ExoTouch/ExoTouchV3.jpeg" textcolor="white"/>
   </slide>
   <slide :index="2" class="slide">
-    <router-link to="/RHIM">
-    <SlideTmplWhite title="Robotic haptic interface" caption='Encounter-type haptic robots for desk-size environnement exploration in virtual reality' image-path="/Internship/SDV1.PNG" textcolor="white"/>
-    </router-link>
+    <SlideTmplWhite title="Robotic haptic interface" routerPath='/rhim' caption='Encounter-type haptic robots for desk-size environnement exploration in virtual reality' image-path="/Internship/SDV1.PNG" textcolor="white"/>
   </slide>
   <slide :index="3" class="slide">
-    <router-link to="/HapticSuit">
-    <SlideTmplWhite title="Environnemental haptic suit (WIP)" caption='Creation of a haptic suit for environmental thermal feedback in virtual reality. Ongoing project.' image-path="/Internship/SDV1.PNG" textcolor="white"/>
-    </router-link>
+    <SlideTmplWhite title="Environnemental haptic suit (WIP)" routerPath='/hapticsuit' caption='Creation of a haptic suit for environmental thermal feedback in virtual reality. Ongoing project.' image-path="/Internship/SDV1.PNG" textcolor="white"/>
   </slide>
   <slide :index="4" class="slide">
-    <router-link to="/Network">
-    <SlideTmplWhite title="HTML/CSS/JS developpement and self web hosting" caption='Creation of websites in HTML/CSS/Vue.js and implementation of network infrastructure to host web sites and services.' image-path="/Hosting/thumbnail.png" textcolor="white"/>
-    </router-link>
+    <SlideTmplWhite title="HTML/CSS/JS developpement and self web hosting" routerPath='/web' caption='Creation of websites in HTML/CSS/Vue.js and implementation of network infrastructure to host web sites and services.' image-path="/Hosting/thumbnail.png" textcolor="white"/>
   </slide>
 </carousel-3d>
   </div>
@@ -84,7 +74,7 @@ export default {
 }
 
 .carousel-3d-controls, .prev, .next {
-  color: white !important;
+  color: #D88F00 !important;
 }
 
 .slide{
