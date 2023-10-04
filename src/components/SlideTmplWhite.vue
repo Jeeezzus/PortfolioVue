8c90bb41f15542fdb4cc58ffeefa224b0d08aad9
Gauthier Gendreau <37701168+Jeeezzus@users.noreply.github.com>
2023-10-04 15:34:19 +0200

    modified carrousel layout

--- src/components/SlideTmplWhite.vue
+++ src/components/SlideTmplWhite.vue
@@ -1,9 +1,15 @@
 <!-- SlideTmpl.vue -->
 <template>
   <div class="slide-tmpl">
-      <img :src="imagePath" alt="Slide Image" class="full-image" />
-    <h2 class="slide-title" :style="{color: titlecolor}">{{ title }}</h2>
-    <p class="slidecap">{{ caption }}</p>
+    <div class="two-column-layout">
+      <div class="column1">
+        <img :src="imagePath" alt="Slide Image" class="full-image" />
+      </div>
+      <div class="column2">
+        <h2 class="slide-title" :style="{color: titlecolor}">{{ title }}</h2>
+      <p class="slidecap">{{ caption }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -25,12 +31,10 @@ export default {
   position: relative;
   text-align: center;
   overflow: hidden; /* Ensure the image doesn't overflow the container */
-  aspect-ratio: 1;
 }
 
 /* Style the image container */
 .image-container {
-  position: relative;
   width: 100%;
   height: 100%;
   object-fit: cover;
@@ -38,9 +42,9 @@ export default {
 
 /* Style the image to be fullscreen */
 .full-image {
-  width: 100%;
-  height: 100%;
-  object-fit: cover; /* Maintain aspect ratio and cover the entire container */
+  width: 100%M !important;
+  aspect-ratio: 1;
+  object-fit: cover;
 }
 
 /* Style the title */
@@ -77,4 +81,36 @@ export default {
   opacity: 0.7; /* Adjust the opacity of the reflection */
 }
 
+.two-column-layout {
+  object-fit: cover;
+  display: flex;
+  width: 100% !important;
+  height: 100% !important; /* Set the height of the container as needed */
+}
+
+.column2 {
+  object-fit: cover;
+  flex: 10;
+  align-items: center;
+  width: 100% !important;
+  height: 100% !important;
+}
+
+.column1 {
+  object-fit: cover;
+  flex: 15;
+  align-items: center;
+  width: 100% !important;
+  height: 100% !important;
+}
+@media (max-width: 750px) {
+  .two-column-layout {
+    flex-direction: column; /* Change to vertical layout */
+    align-items: center; /* Center horizontally in vertical layout */
+  }
+
+  .slide-title{
+    height: 80vh;
+  }
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,30 +1,29 @@
 <template>
   <div class="home">
-    <div class="carou"></div>
   <div class="carou">
   <carousel-3d :controls-visible="true" :controls-prev-html="'&#10092; '" :controls-next-html="'&#10093;'" 
-               :controls-width="30" :controls-height="60" :height="heightInPixels" :width="heightInPixels" :autoplay="true" :autoplay-timeout="4000" >
-  <slide :index="0">
+               :controls-width="30" :controls-height="60" :height="heightInPixels" :width="heightInPixels*1.75"  >
+  <slide :index="0" class="slide">
     <router-link to="/DVB">
     <SlideTmplWhite title="InMoov and Jasper" caption='Development then project management for DavinciBot in the creation of the humanoid robots "InMoov" and "JASPER"' image-path="/DVB/InMoov_Working.png" textcolor="white"/>
     </router-link>
   </slide>
-  <slide :index="1">
+  <slide :index="1" class="slide">
     <router-link to="/ExoTouch">
     <SlideTmplWhite title="ExoTouch haptic gloves" caption='Creation of haptic gloves with force feedback for virtual reality, DIY and inexpensive.' image-path="/ExoTouch/ExoTouchV3.jpeg" textcolor="white"/>
     </router-link>
   </slide>
-  <slide :index="2">
+  <slide :index="2" class="slide">
     <router-link to="/RHIM">
     <SlideTmplWhite title="Robotic haptic interface" caption='Encounter-type haptic robots for desk-size environnement exploration in virtual reality' image-path="/Internship/SDV1.PNG" textcolor="white"/>
     </router-link>
   </slide>
-  <slide :index="3">
+  <slide :index="3" class="slide">
     <router-link to="/HapticSuit">
-    <SlideTmplWhite title="Environnemental haptic suit (WIP)" caption='Creation of a haptic suit for environmental thermal feedback in virtual reality. Ongoing project.' image-path="" textcolor="white"/>
+    <SlideTmplWhite title="Environnemental haptic suit (WIP)" caption='Creation of a haptic suit for environmental thermal feedback in virtual reality. Ongoing project.' image-path="/Internship/SDV1.PNG" textcolor="white"/>
     </router-link>
   </slide>
-  <slide :index="4">
+  <slide :index="4" class="slide">
     <router-link to="/Network">
     <SlideTmplWhite title="HTML/CSS/JS developpement and self web hosting" caption='Creation of websites in HTML/CSS/Vue.js and implementation of network infrastructure to host web sites and services.' image-path="/Hosting/thumbnail.png" textcolor="white"/>
     </router-link>
@@ -35,6 +34,7 @@
 </template>
 
 <script>
+/*:autoplay="true" :autoplay-timeout="4000"*/
 
 import { Carousel3d, Slide } from 'vue-carousel-3d';
 import SlideTmplWhite from '@/components/SlideTmplWhite.vue';
@@ -48,7 +48,7 @@ export default {
   mounted() {
     // Calculate 70% of the viewport height in pixels
     const viewportHeight = window.innerHeight;
-    this.heightInPixels = Math.floor(viewportHeight * 0.6);
+    this.heightInPixels = Math.floor(viewportHeight * 0.75);
   },
   components: {
     Carousel3d,
@@ -71,6 +71,10 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
+  padding-bottom: 500px;
+  height: 800px;
+  
+  z-index:1000;
 }
 
 .home{
@@ -79,5 +83,31 @@ export default {
   overflow: hidden;
 }
 
+.carousel-3d-controls, .prev, .next {
+  color: white !important;
+}
 
+.slide{
+  /*box-shadow: 0px 5vh 5vh #bebebebe;*/
+  border-radius: 20px;
+  left: 10% !important;
+  right: 10% !important;
+  height: 80% !important;
+  width: 80% !important;
+}
+
+@media (max-width: 750px) {
+  .slide{
+    height: 80vh !important;
+
+  }
+
+  .carou{
+    top:10%;
+  }
+
+  .carousel-3d-container{
+    height: 90vh !important;
+  }
+}
 </style>
